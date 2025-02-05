95330460e9197ab05fcb4512d621079559bac0dc
Luiz Oliveira <luizoliveira2808@gmail.com>
2025-02-05 20:48:17 -0300

    fix: layout shift

--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -6,10 +6,15 @@ export default async function Layout({
   children: React.ReactNode;
 }) {
   return (
-    <div>
+    <div
+      className="h-svh overflow-y-auto [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500 [&::-webkit-scrollbar-track]:bg-gray-100 dark:[&::-webkit-scrollbar-track]:bg-neutral-700 [&::-webkit-scrollbar]:w-2"
+      style={{ scrollbarGutter: "stable" }}
+    >
       <AppHeader />
 
-      <div className="container mx-auto p-4 space-y-4">{children}</div>
+      <div>
+        <div className="container mx-auto space-y-4 p-4">{children}</div>
+      </div>
     </div>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className="antialiased [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500 [&::-webkit-scrollbar-track]:bg-gray-100 dark:[&::-webkit-scrollbar-track]:bg-neutral-700 [&::-webkit-scrollbar]:w-2">
+      <body className="antialiased">
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
